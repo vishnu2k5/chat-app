36340bca07d59390c514f107b6812e0f918bd5ce
vishnu2k5 <mvishnuvardhan156@gmial.com>
2025-10-06 12:19:45 +0530

    dip

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
-
+import path from "path"
 import authRouter from './routes/auth.route.js';
 import messageroute from './routes/messages.route.js'
 
@@ -8,6 +8,8 @@ import messageroute from './routes/messages.route.js'
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+//making ready for diployment 
+const __dirname = path.resolve()
 
 app.use('/api/auth',authRouter)
 app.use('/api/message',messageroute)
@@ -24,6 +26,12 @@ app.use('/api/message',messageroute)
 
 
 
-
+//if production ready diploy
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    });
+}
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
